b9bfa04fb8974bc49c4784b6dcf85fe98d436cf0
6a696d33 <jimmy3.xyz@gmail.com>
2025-02-11 13:49:41 -0500

    Update network-scanner.py

--- network-scanner.py
+++ network-scanner.py
@@ -17,11 +17,10 @@ def nmap_scan(cidr):
         sys.exit(1)
 
 
-def parse_nmap_output(output):
+def parser(output):
     if not isinstance(output, str):
         print("Error: Output is not a string!")
         return
-
     lst = []
     regexObj = re.compile(r"\d{2,5}/")
     try:
@@ -32,7 +31,6 @@ def parse_nmap_output(output):
     except re.error as e:
         print(f"Regex error: {e}")
         return
-
     return lst
 
 
@@ -40,24 +38,21 @@ def main():
     if len(sys.argv) < 2:
         print("Usage: " + sys.argv[0] + "<base IP address> <subnet mask>")
         sys.exit(1)
-
     ipAddr = sys.argv[1]
     ipAddrRegex = re.compile(r"(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})")
     ipAddrMatch = ipAddrRegex.match(ipAddr)
     if not ipAddrMatch:
         print("Invalid IP address")
         sys.exit(1)
-
     subnetMask = sys.argv[2]
     subnetMaskRegex = re.compile(r"\d{1,2}")
     subnetMaskMatch = subnetMaskRegex.match(subnetMask)
     if not subnetMaskMatch:
         print("Invalid subnet mask")
         sys.exit(1)
-
     cidr = ipAddr + "/" + subnetMask
     output = nmap_scan(cidr)
-    portLst = parse_nmap_output(output)
+    portLst = parser(output)
     print(f"{portLst}")
 
 
